61a2532fb7614ea321d075272eb17a08d04e8bbe
Andy Cobaugh <phalenor@gmail.com>
2010-02-16 17:24:55 -0500

    display acl key based on $wgACLNames display effective acl using efACLCumulativeACL() display acls for each individual category efACLWikiTextACL() : display (none) if $acl is empty

--- ACL/ACLTab.php
+++ ACL/ACLTab.php
@@ -34,7 +34,7 @@ function efACLHookContentTab(&$content_actions)
 /* display the tab */
 function efACLDisplayTab($action, &$wgArticle)
 {
-	global $wgOut, $wgUser;
+	global $wgOut, $wgUser, $wgACLNames;
 
 	if ($action == 'acl') {
 		
@@ -52,12 +52,21 @@ function efACLDisplayTab($action, &$wgArticle)
 		$category_acl = efACLCategoryACL($title);
 		$ns_acl = efACLNamespaceACL($title);
 
-		$text = "== User ==\n";
+		$text = "<div style=\"float:right; clear:right;margin-left: 1em; background-color:#f3f3ff; border:1px solid; padding-left: 0.5em; padding-right: 0.5em\">\n";
+		$text .= "'''Key'''\n";
+		foreach ($wgACLNames as $k => $v) {
+			$text .= "* ''$k'' = $v\n";
+		}
+		$text .= "</div>\n";
+
+		$text .= "== User ==\n";
 		$text .= "* Username: $username\n";
 		$text .= "* Groups: ";
 		$text .= implode(', ', $groups);
 		$text .= "\n";
 
+		$text .= "== Effective ACLs ==\n";
+		$text .= efACLWikiTextACL(efACLCumulativeACL($title), 1);
 		$text .= "== Page ACLs ==\n";
 		$text .= "ACLs from this page:\n";
 		$text .= efACLWikiTextACL($page_acl, 1);
@@ -67,8 +76,18 @@ function efACLDisplayTab($action, &$wgArticle)
 		$text .= efACLWikiTextACL($ns_acl, 1);
 
 		$text .= "== Category ACLs ==\n";
-		$text .="ACLs from categories:\n";
+		$text .="Total ACLs from all categories:\n";
 		$text .= efACLWikiTextACL($category_acl, 1);
+		
+		$category_tree = $title->getParentCategoryTree();
+		if (count($category_tree) > 0) {
+			$category_tree_flat = efACLFlattenCategoryTree($category_tree);
+			foreach ($category_tree_flat as $category) {
+				$category_title = Title::newFromText($category);
+				$text .= "=== $category ===\n";
+				$text .= efACLWikiTextACL(efACLTitleACL($category_title));
+			}
+		}
 		
 		$wgOut->addWikiText($text);
 	}
@@ -76,18 +95,22 @@ function efACLDisplayTab($action, &$wgArticle)
 	return false;
 }
 
-function efACLWikiTextACL($acl, $indent) {
+function efACLWikiTextACL($acl, $indent = 0) {
 	$text = '';
 	$prefix = '';
 
-	if ($indent > 0) {
-		for ($i = 0; $i < $indent; $i++) {
-			$prefix .= '*';
+	if (count($acl) > 0) {
+		if ($indent > 0) {
+			for ($i = 0; $i < $indent; $i++) {
+				$prefix .= '*';
+			}
 		}
-	}
 
-	foreach ($acl as $entity => $value) {
-		$text .= $prefix . $entity  . ' : ' . implode($value) . "\n";
+		foreach ($acl as $entity => $value) {
+			$text .= $prefix . $entity  . ' : ' . implode($value) . "\n";
+		}
+	} else {
+		$text .= $prefix . "(none)\n";
 	}
 	
 	return $text;
